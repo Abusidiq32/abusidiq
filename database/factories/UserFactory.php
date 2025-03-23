d08e8d7862ddb7bd2f1bff93c0bcede040ceb7f0
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2025-03-23 06:31:01 +0000

    seeded a user to the table for use

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,10 +24,10 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
+            'password' => '$2y$12$Awhi6Yy..1/TGwgclXrxEe7PeR8yPolSyDw4eX.quBy5jR9TWwaZi', // password
             'remember_token' => Str::random(10),
         ];
     }
