42cc4157f295aca1e1c8c4bdf5057b8668c63fb2
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2025-03-24 05:51:58 +0000

    frontend page added and divided into sections also with its layer(navbar and footer)

---
+++ resources/views/frontend/sections/portfolio.blade.php
@@ -0,0 +1,45 @@
+<section class="portfolio-area section-padding-top" id="portfolio-page">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 offset-lg-3 text-center">
+                <div class="section-title">
+                    <h3 class="title">Latest Portfolio</h3>
+                    <div class="desc">
+                        <p>Earum quos animi numquam excepturi eveniet explicabo repellendus rem esse. Quae quasi
+                            odio enim.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-12">
+                <ul class="filter-menu">
+                    <li class="active" data-filter="*">All Projects</li>
+                    <li data-filter=".branding">Branding Design</li>
+                    <li data-filter=".interface">User Interface</li>
+                    <li data-filter=".experience">User Experience</li>
+                    <li data-filter=".development">Web Development</li>
+                </ul>
+            </div>
+        </div>
+        <div class="portfolio-wrapper">
+            <div class="row portfolios">
+                <div data-wow-delay="0.3s" class="col-md-6 col-lg-4 filter-item branding">
+                    <div class="single-portfolio">
+                        <figure class="portfolio-image">
+                            <img src="{{asset('frontend/assets/images/portfolio-1.jpg')}}" alt="">
+                        </figure>
+                        <div class="portfolio-content">
+                            <a href="images/portfolio-1.jpg" data-lity class="icon"><i
+                                    class="fas fa-plus"></i></a>
+                            <h4 class="title"><a href="portfolio-details.html">Black Golden</a></h4>
+                            <div class="desc">
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
