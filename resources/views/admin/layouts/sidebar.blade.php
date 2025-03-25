403de8b6dc7ee1736157ee8811a996eb86cb1c57
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2025-03-25 23:22:16 +0000

    added li for service in admin sidebar

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -60,12 +60,14 @@
                 <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i>
                     <span>Hero</span></a>
                 <ul class="dropdown-menu" style="display: none;">
-                    <li><a class="nav-link" href="{{route('admin.hero.index')}}">Hero section</a></li>
                     <li><a class="nav-link" href="{{route('admin.typer-title.index')}}">Typer Title </a></li>
+                    <li><a class="nav-link" href="{{route('admin.hero.index')}}">Hero section</a></li>
 
                 </ul>
             </li>
 
+            <li><a class="nav-link" href="{{route('admin.service.index')}}"><i class="far fa-square"></i> <span>Service</span></a></li>
+            
             {{-- <li><a class="nav-link" href="blank.html"><i class="far fa-square"></i> <span>Blank Page</span></a></li> --}}
 
 
