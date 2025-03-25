20dfe8351f34d537219dfc7cbdb421ecc715ebc4
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2025-03-25 14:25:35 +0000

    route for typertitle added with its controller class

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Admin\dashboardController;
 use App\Http\Controllers\Admin\HeroController;
 use App\Http\Controllers\Admin\ProfileController;
+use App\Http\Controllers\Admin\TyperTitleController;
 use App\Http\Controllers\Frontend\HomeController;
 use Illuminate\Support\Facades\Route;
 
@@ -50,6 +51,7 @@ Route::middleware('auth')->group(function () {
 // group for admin routes
 Route::group(['middleware' => ['auth'], 'prefix' => 'admin', 'as' => 'admin.'], function(){
     Route::resource('hero', HeroController::class);
+    Route::resource('typer-title', TyperTitleController::class);
 });
 
 require __DIR__.'/auth.php';
