264e9ac4964d725561f03816d79a223cef80748a
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2025-03-28 18:10:49 +0000

    created helper folder and helper file for rerusable functions

---
+++ app/Helper/helpers.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\File;
+
+
+
+// Handle file upload
+function handleUpload($inputName, $model = null)
+{
+    try {
+        if (request()->hasFile($inputName)) {
+            if ($model && File::exists(public_path($model->$inputName))) {
+                File::delete(public_path($model->$inputName));
+            }
+
+            $file = request()->file($inputName);
+            $fileName = time() . '.' . $file->getClientOriginalName();
+            $file->move(public_path('/uploads'), $fileName);
+
+            $filePath = '/uploads/' . $fileName;
+            return $filePath;
+        }
+    } catch (\Exception $e) {
+        throw $e;
+    }
+}
