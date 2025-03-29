8665835a4d5296006b78d74e4df22081989105d0
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2025-03-29 12:38:19 +0000

    view file added

---
+++ resources/views/admin/portfolio-category/edit.blade.php
@@ -0,0 +1,44 @@
+@extends('admin.layouts.layout')
+
+@section('content')
+    <!-- Main Content -->
+        <section class="section">
+            <div class="section-header">
+                <div class="section-header-back">
+                    <a href="{{route('dashboard')}}" class="btn btn-icon"><i class="fas fa-arrow-left"></i></a>
+                </div>
+                <h1>Portfolio Category Section</h1>
+            </div>
+
+            <div class="section-body">
+
+                <div class="row">
+                    <div class="col-12">
+                        <div class="card">
+                            <div class="card-header">
+                                <h4>Edit Category</h4>
+                            </div>
+                            <div class="card-body">
+                                <form action="{{route('admin.portfolio-category.update', $category->id)}}" method="post" enctype="multipart/form-data">
+                                    @csrf
+                                    @method('PUT')
+                                    <div class="form-group row mb-4">
+                                        <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Name</label>
+                                        <div class="col-sm-12 col-md-7">
+                                            <input type="text" name="name" class="form-control" value="{{$category->name}}">
+                                        </div>
+                                    </div>
+                                    <div class="form-group row mb-4">
+                                        <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3"></label>
+                                        <div class="col-sm-12 col-md-7 text-right">
+                                          <button class="btn btn-primary">Update Category</button>
+                                        </div>
+                                </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+@endsection
