ce6cf0bc2c065e00e1858a28f8ab819a85606112
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2025-03-29 12:36:44 +0000

    sidebar link for portfolio cat added

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -69,6 +69,16 @@
             <li><a class="nav-link" href="{{route('admin.service.index')}}"><i class="far fa-square"></i> <span>Service</span></a></li>
 
             <li><a class="nav-link" href="{{route('admin.about.index')}}"><i class="far fa-square"></i> <span>About</span></a></li>
+
+            <li class="nav-item dropdown">
+                <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i>
+                    <span>Portfolio</span></a>
+                <ul class="dropdown-menu" style="display: none;">
+                    <li><a class="nav-link" href="{{route('admin.portfolio-category.index')}}">Category</a></li>
+                    <li><a class="nav-link" href="{{route('admin.hero.index')}}">Projects</a></li>
+
+                </ul>
+            </li>
             
             {{-- <li><a class="nav-link" href="blank.html"><i class="far fa-square"></i> <span>Blank Page</span></a></li> --}}
 
