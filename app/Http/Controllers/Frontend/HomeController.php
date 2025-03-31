8149807042e15f32cb583329076d71e49b94a232
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2025-04-01 00:49:06 +0100

    portfoliosettings, categories and item model added and showportfolio route return its view

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers\Frontend;
 use App\Http\Controllers\Controller;
 use App\Models\About;
 use App\Models\Hero;
+use App\Models\PortfolioCategory;
+use App\Models\PortfolioItem;
+use App\Models\PortfolioSettings;
 use App\Models\Service;
 use App\Models\TyperTitle;
 use Illuminate\Http\Request;
@@ -17,6 +20,23 @@ class HomeController extends Controller
         $typerTitles = TyperTitle::all();
         $services = Service::all();
         $about = About::first();
-        return view('frontend.home', compact('hero', 'typerTitles', 'services', 'about'));
+        $portfolioSettings = PortfolioSettings::first();
+        $portfolioCategories = PortfolioCategory::all();
+        $portfolioItems = PortfolioItem::where('status', 'published')->get();
+        return view('frontend.home', 
+                compact(
+                    'hero', 
+                    'typerTitles', 
+                    'services', 
+                    'about', 
+                    'portfolioSettings', 
+                    'portfolioCategories',
+                    'portfolioItems'
+                ));
+    }
+
+    function showPortfolio($id){
+        $portfolioItem = PortfolioItem::findOrFail($id);
+        return view('frontend.portfolio-details', compact('portfolioItem'));
     }
 }
