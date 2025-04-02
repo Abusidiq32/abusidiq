bf0681891455bfb03504f9b50f137fdb6c710190
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2025-04-02 20:35:03 +0100

    sidebard link for skills settings section added

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -80,6 +80,17 @@
 
                 </ul>
             </li>
+
+            <li class="nav-item dropdown">
+                <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i>
+                    <span>Skills</span></a>
+                <ul class="dropdown-menu" style="display: none;">
+                    {{-- <li><a class="nav-link" href="{{route('admin.portfolio-category.index')}}">Portfolio Category</a></li>
+                    <li><a class="nav-link" href="{{route('admin.portfolio-item.index')}}">Portfolio Items</a></li> --}}
+                    <li><a class="nav-link" href="{{route('admin.skills-settings.index')}}">Skills Settings</a></li>
+
+                </ul>
+            </li>
             
             {{-- <li><a class="nav-link" href="blank.html"><i class="far fa-square"></i> <span>Blank Page</span></a></li> --}}
 
