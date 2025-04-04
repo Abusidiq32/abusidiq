28fa2d69a4a789199b8143e772a54c58d928e04b
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2025-04-04 15:05:54 +0100

    frontend blog details and list page modified

--- resources/views/frontend/sections/blog.blade.php
+++ resources/views/frontend/sections/blog.blade.php
@@ -1,3 +1,5 @@
+@if ($blogs->count())
+    
 <section class="blog-area section-padding-top" id="blog-page">
     <div class="container">
         <div class="row">
@@ -13,97 +15,26 @@
         <div class="row">
             <div class="col-sm-12">
                 <div class="blog-slider">
+                    @foreach ($blogs as $blog)
+                        
                     <div class="single-blog">
                         <figure class="blog-image">
-                            <img src="images/blog-1.jpg" alt="">
+                            <img src="{{asset($blog->image)}}" alt="">
                         </figure>
                         <div class="blog-content">
-                            <h3 class="title"><a href="blog-details.html">Great things never come from.</a></h3>
+                            <h3 class="title"><a href="{{route('blog.details', $blog->id)}}">{{$blog->title}}</a></h3>
                             <div class="desc">
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
-                                    consequuntur provident iusto cumque . </p>
+                                <p>{{ Str::limit(strip_tags($blog->description), 150, '...') }}</p>
                             </div>
-                            <a href="#" class="button-primary-trans mouse-dir">Read More <span
+                            <a href="{{route('blog.details', $blog->id)}}" class="button-primary-trans mouse-dir">Read More <span
                                     class="dir-part"></span> <i class="fal fa-arrow-right"></i></a>
                         </div>
                     </div>
-                    <div class="single-blog">
-                        <figure class="blog-image">
-                            <img src="images/blog-2.jpg" alt="">
-                        </figure>
-                        <div class="blog-content">
-                            <h3 class="title"><a href="blog-details.html">Don’t stop when you’re tired. Stop
-                                    when.</a></h3>
-                            <div class="desc">
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
-                                    consequuntur provident iusto cumque . </p>
-                            </div>
-                            <a href="#" class="button-primary-trans mouse-dir">Read More <span
-                                    class="dir-part"></span> <i class="fal fa-arrow-right"></i></a>
-                        </div>
-                    </div>
-                    <div class="single-blog">
-                        <figure class="blog-image">
-                            <img src="images/blog-3.jpg" alt="">
-                        </figure>
-                        <div class="blog-content">
-                            <h3 class="title"><a href="blog-details.html">Success doesn’t just find you.</a>
-                            </h3>
-                            <div class="desc">
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
-                                    consequuntur provident iusto cumque . </p>
-                            </div>
-                            <a href="#" class="button-primary-trans mouse-dir">Read More <span
-                                    class="dir-part"></span> <i class="fal fa-arrow-right"></i></a>
-                        </div>
-                    </div>
-                    <div class="single-blog">
-                        <figure class="blog-image">
-                            <img src="images/blog-4.jpg" alt="">
-                        </figure>
-                        <div class="blog-content">
-                            <h3 class="title"><a href="blog-details.html">Sometimes later becomes never.</a>
-                            </h3>
-                            <div class="desc">
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
-                                    consequuntur provident iusto cumque . </p>
-                            </div>
-                            <a href="#" class="button-primary-trans mouse-dir">Read More <span
-                                    class="dir-part"></span> <i class="fal fa-arrow-right"></i></a>
-                        </div>
-                    </div>
-                    <div class="single-blog">
-                        <figure class="blog-image">
-                            <img src="images/blog-5.jpg" alt="">
-                        </figure>
-                        <div class="blog-content">
-                            <h3 class="title"><a href="blog-details.html">Don’t wait for opportunity. Create
-                                    it.</a></h3>
-                            <div class="desc">
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
-                                    consequuntur provident iusto cumque . </p>
-                            </div>
-                            <a href="#" class="button-primary-trans mouse-dir">Read More <span
-                                    class="dir-part"></span> <i class="fal fa-arrow-right"></i></a>
-                        </div>
-                    </div>
-                    <div class="single-blog">
-                        <figure class="blog-image">
-                            <img src="images/blog-6.jpg" alt="">
-                        </figure>
-                        <div class="blog-content">
-                            <h3 class="title"><a href="blog-details.html">Your limitation-it’s only your
-                                    imagination.</a></h3>
-                            <div class="desc">
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
-                                    consequuntur provident iusto cumque . </p>
-                            </div>
-                            <a href="#" class="button-primary-trans mouse-dir">Read More <span
-                                    class="dir-part"></span> <i class="fal fa-arrow-right"></i></a>
-                        </div>
-                    </div>
+                    @endforeach
+
                 </div>
             </div>
         </div>
     </div>
 </section>
+@endif
