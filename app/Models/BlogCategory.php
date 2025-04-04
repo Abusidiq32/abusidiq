75ee537898c348da281e6709551de1d620528a93
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2025-04-04 15:03:30 +0100

    method to handle the hasmany request frrom blog category

--- app/Models/BlogCategory.php
+++ app/Models/BlogCategory.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class BlogCategory extends Model
 {
     use HasFactory;
+    public function items()
+    {
+        return $this->hasMany(Blog::class, 'category_id');
+    }
 }
