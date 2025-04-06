c0e175d5de23f89e5761a0ff8a185277ddba3c7a
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2025-04-06 12:26:29 +0100

    crud page for footer-social links using an api to fetch font awesome

---
+++ resources/views/admin/footer-social/edit.blade.php
@@ -0,0 +1,51 @@
+@extends('admin.layouts.layout')
+
+@section('content')
+    <!-- Main Content -->
+        <section class="section">
+            <div class="section-header">
+                <div class="section-header-back">
+                    <a href="{{route('admin.footer-social.index')}}" class="btn btn-icon"><i class="fas fa-arrow-left"></i></a>
+                </div>
+                <h1>Footer Social Link</h1>
+            </div>
+
+            <div class="section-body">
+
+                <div class="row">
+                    <div class="col-12">
+                        <div class="card">
+                            <div class="card-header">
+                                <h4>Edit Footer Social Link</h4>
+                            </div>
+                            <div class="card-body">
+                                <form action="{{route('admin.footer-social.update', $socialLink->id)}}" method="post" enctype="multipart/form-data">
+                                    @csrf
+                                    @method('PUT')
+                                    <div class="form-group row mb-4">
+                                        <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Icon</label>
+                                        <div class="col-sm-12 col-md-7">
+                                            <button name="icon" class="btn btn-secondary" role="iconpicker" data-iconset="fontawesome5" data-icon="{{$socialLink->icon}}"></button>
+                                        </div>
+                                    </div>
+                                    <div class="form-group row mb-4">
+                                        <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Url</label>
+                                        <div class="col-sm-12 col-md-7">
+                                            <input type="text" name="url" class="form-control" value="{{$socialLink->url}}">
+                                        </div>
+                                    </div>
+                                    <div class="form-group row mb-4">
+                                        <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3"></label>
+                                        <div class="col-sm-12 col-md-7 text-right">
+                                            <button class="btn btn-primary">Create</button>
+                                        </div>
+                                    </div>
+                                </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+@endsection
