65af2dcf9bf5b6a4763d7936dce73a61c7ca3cac
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2025-04-08 23:34:13 +0100

    models facade added dahsboardController using count to be be able to display its total number in the front end

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -10,14 +10,14 @@
             <div class="col-lg-3 col-md-6 col-sm-6 col-12">
                 <div class="card card-statistic-1">
                     <div class="card-icon bg-primary">
-                        <i class="far fa-user"></i>
+                        <i class="far fa-newspaper"></i>
                     </div>
                     <div class="card-wrap">
                         <div class="card-header">
-                            <h4>Total Admin</h4>
+                            <h4>Total Blogs</h4>
                         </div>
                         <div class="card-body">
-                            10
+                            {{$blogCount}}
                         </div>
                     </div>
                 </div>
@@ -25,14 +25,14 @@
             <div class="col-lg-3 col-md-6 col-sm-6 col-12">
                 <div class="card card-statistic-1">
                     <div class="card-icon bg-danger">
-                        <i class="far fa-newspaper"></i>
+                        <i class="fas fa-laptop-code"></i>
                     </div>
                     <div class="card-wrap">
                         <div class="card-header">
-                            <h4>News</h4>
+                            <h4>Portfolios</h4>
                         </div>
                         <div class="card-body">
-                            42
+                            {{$portfolioCount}}
                         </div>
                     </div>
                 </div>
@@ -40,14 +40,13 @@
             <div class="col-lg-3 col-md-6 col-sm-6 col-12">
                 <div class="card card-statistic-1">
                     <div class="card-icon bg-warning">
-                        <i class="far fa-file"></i>
+                        <i class="fas fa-star"></i>
                     </div>
                     <div class="card-wrap">
                         <div class="card-header">
-                            <h4>Reports</h4>
                         </div>
                         <div class="card-body">
-                            1,201
+                            {{$feedbackCount}}
                         </div>
                     </div>
                 </div>
@@ -55,14 +54,14 @@
             <div class="col-lg-3 col-md-6 col-sm-6 col-12">
                 <div class="card card-statistic-1">
                     <div class="card-icon bg-success">
-                        <i class="fas fa-circle"></i>
+                        <i class="fas fa-thumbs-up"></i>
                     </div>
                     <div class="card-wrap">
                         <div class="card-header">
-                            <h4>Online Users</h4>
+                            <h4>Skills</h4>
                         </div>
                         <div class="card-body">
-                            47
+                            {{$skillsCount}}
                         </div>
                     </div>
                 </div>
