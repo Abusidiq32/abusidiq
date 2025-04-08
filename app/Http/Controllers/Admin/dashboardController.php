548e4e212ed2e79910ed544d827d8fd4186e6e62
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2025-04-08 23:35:45 +0100

    models facade added dahsboardController using count to be be able to display its total number in the front end

--- app/Http/Controllers/Admin/dashboardController.php
+++ app/Http/Controllers/Admin/dashboardController.php
@@ -3,12 +3,25 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Blog;
+use App\Models\Feedback;
+use App\Models\PortfolioItem;
+use App\Models\SkillsItem;
 use Illuminate\Http\Request;
 
 class dashboardController extends Controller
 {
     //
     function index(){
-        return view('admin.dashboard');
+        $blogCount = Blog::count();
+        $portfolioCount = PortfolioItem::count();
+        $feedbackCount = Feedback::count();
+        $skillsCount = SkillsItem::count();
+        return view('admin.dashboard', compact(
+            'blogCount',
+            'portfolioCount',
+            'feedbackCount',
+            'skillsCount'
+        ));
     }
 }
