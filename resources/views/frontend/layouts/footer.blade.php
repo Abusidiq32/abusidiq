22cb93148b005ef760cd40ff1b90cde53b293182
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2025-04-08 08:02:28 +0100

    display data dynamically in the footer using  the model facade at the top

--- resources/views/frontend/layouts/footer.blade.php
+++ resources/views/frontend/layouts/footer.blade.php
@@ -1,44 +1,57 @@
+@php
+    $uefulLinks = \App\Models\FooterUsefulLinks::all();
+    $SocialLinks = \App\Models\FooterSocialLinks::all();
+    $HelpLinks = \App\Models\FooterHelp::all();
+    $contactInfo = \App\Models\FooterContact::first();
+    $Info = \App\Models\FooterInfo::first();
+
+@endphp
+
+
+
 <footer class="footer-area">
     <div class="container">
         <div class="row footer-widgets">
-            <div class="col-md-12 col-lg-3 widget">
-                <div class="text-box">
-                    <figure class="footer-logo">
-                        <img src="images/logo.png" alt="">
-                    </figure>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, libero. </p>
-                    <ul class="d-flex flex-wrap">
-                        <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-                        <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
-                        <li><a href="#"><i class="fab fa-twitter"></i></a></li>
-                        <li><a href="#"><i class="fab fa-behance"></i></a></li>
-                    </ul>
+            @if ($SocialLinks)
+                <div class="col-md-12 col-lg-3 widget">
+                    <div class="text-box">
+                        <figure class="footer-logo">
+                            <img src="{{asset($webSettings->logo)}}" alt="">
+                        </figure>
+                        <p>{{$Info->info}}</p>
+                        <ul class="d-flex flex-wrap">
+                            @foreach ($SocialLinks as $SocialLink)
+                                
+                            <li><a href="{{$SocialLink->url}}" target="_blank"><i class="{{$SocialLink->icon}}"></i></a></li>
+                            @endforeach
+                        </ul>
+                    </div>
                 </div>
-            </div>
+            @endif
+
             <div class="col-md-4 col-lg-2 offset-lg-1 widget">
                 <h3 class="widget-title">Useful Link</h3>
                 <ul class="nav-menu">
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Portfolio</a></li>
-                    <li><a href="#">Blog</a></li>
+                    @foreach ($uefulLinks as $uefulLink)
+                        <li><a href="{{$uefulLink->url}}">{{$uefulLink->name}}</a></li>
+                    @endforeach
+
                 </ul>
             </div>
             <div class="col-md-4 col-lg-3 widget">
                 <h3 class="widget-title">Contact Info</h3>
                 <ul>
-                    <li>17232 Broadway Suite 308, Jackson Heights, 11372, NY, United States.</li>
-                    <li><a href="#">+1347-430-9510</a></li>
-                    <li><a href="#">abusidiq@gmail.com</a></li>
+                    <li>{{ $contactInfo->address}}</li>
+                    <li><a href="#">{{$contactInfo->phone}}</a></li>
+                    <li><a href="#">{{$contactInfo->email}}</a></li>
                 </ul>
             </div>
             <div class="col-md-4 col-lg-3 widget">
                 <h3 class="widget-title">Help</h3>
                 <ul class="nav-menu">
-                    <li><a href="#">Privacy Policy</a></li>
-                    <li><a href="#">404 Page</a></li>
-                    <li><a href="#">Terms</a></li>
-                    <li><a href="#">Documentation</a></li>
+                    @foreach ($HelpLinks as $HelpLink)
+                        <li><a href="{{$HelpLink->url}}" target="_blank">{{$HelpLink->name}}</a></li>
+                    @endforeach
                 </ul>
             </div>
         </div>
@@ -48,8 +61,9 @@
             <div class="row">
                 <div class="col-12">
                     <div class="copyright">
-                        <p>Copyright 2025 <span>Abusidiq-digitals</span> All Rights Reserved.</p>
-                        <p>Powered by Abusidiq &nbsp; | &nbsp; 2024 - 2025</p>
+                        {{-- <p>Copyright 2025 <span>Abusidiq-digitals</span> All Rights Reserved.</p> --}}
+                        <p>{{$Info->copyright}}</p>
+                        <p>{{$Info->powered_by}}</p>
                     </div>
                 </div>
             </div>
