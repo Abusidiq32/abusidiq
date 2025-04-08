07f5dfc7c7f591752ee297bf40eb223120e8b7c7
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2025-04-08 08:00:39 +0100

    seo and web settings page created

---
+++ resources/views/admin/general-setttings/web-settings/index.blade.php
@@ -0,0 +1,70 @@
+@extends('admin.layouts.layout')
+
+@section('content')
+    <!-- Main Content -->
+    <section class="section">
+        <div class="section-header">
+            <div class="section-header-back">
+                <a href="{{ route('dashboard') }}" class="btn btn-icon"><i class="fas fa-arrow-left"></i></a>
+            </div>
+            <h1>Web Settings Section</h1>
+        </div>
+
+        <div class="section-body">
+
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h4>Update Web Settings</h4>
+                        </div>
+                        <div class="card-body">
+                            <form action="{{ route('admin.web-settings.update', 1) }}" method="post"
+                                enctype="multipart/form-data">
+                                @csrf
+                                @method('PUT')
+                                <div class="form-group row mb-4">
+                                    <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Preview Favicon</label>
+                                    <div class="col-sm-12 col-md-7">
+                                        <img style="width:50px" src="{{asset($webSettings->favicon)}}" class="w-25" alt="">
+                                    </div>
+                                </div>
+                                <div class="form-group row mb-4">
+                                    <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Favicon</label>
+                                    <div class="col-sm-12 col-md-7">
+                                        <div class="custom-file">
+                                            <input type="file" name="favicon" class="custom-file-input" id="customFile">
+                                            <label class="custom-file-label" for="customFile">Choose file</label>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="form-group row mb-4">
+                                    <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Preview Logo</label>
+                                    <div class="col-sm-12 col-md-7">
+                                        <img src="{{asset($webSettings->logo)}}" class="w-25" alt="">
+                                    </div>
+                                </div>
+                                <div class="form-group row mb-4">
+                                    <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Logo</label>
+                                    <div class="col-sm-12 col-md-7">
+                                        <div class="custom-file">
+                                            <input type="file" name="logo" class="custom-file-input" id="customFile">
+                                            <label class="custom-file-label" for="customFile">Choose file</label>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="form-group row mb-4">
+                                    <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3"></label>
+                                    <div class="col-sm-12 col-md-7 text-right">
+                                        <button class="btn btn-primary">Save Changes</button>
+                                    </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+    </section>
+@endsection
