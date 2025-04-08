d9906a5f3bf414511eacf78f4e742ed2eda1dba9
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2025-04-08 07:59:59 +0100

    seo settings controller, model and database migration created

---
+++ app/Http/Controllers/Admin/SeoSettingsController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\SeoSettings;
+use Illuminate\Http\Request;
+
+class SeoSettingsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $seoSettings = SeoSettings::first();
+        return view('admin.general-setttings.seo-settings.index', compact('seoSettings'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $request->validate([
+            'title' => ['required', 'max:255'],
+            'description' => ['required', 'max:1000'],
+            'keywords' => ['required', 'max:300'],
+        ]);
+
+        SeoSettings::updateOrCreate(
+            ['id' => $id],
+            [
+                'title' => $request->title,
+                'description' => $request->description,
+                'keywords' => $request->keywords,
+            ]
+        );
+        toastr()->success('SEO Settings Updated Successfully');
+        return redirect()->back();
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
