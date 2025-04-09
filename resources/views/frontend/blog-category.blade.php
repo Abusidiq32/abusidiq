c1c5ab939f245756da857c7241c84a35e7f35020
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2025-04-10 00:47:30 +0100

    Update breadcrumb navigation in blog category page to reflect current category

--- resources/views/frontend/blog-category.blade.php
+++ resources/views/frontend/blog-category.blade.php
@@ -9,7 +9,7 @@
                 <div class="col-sm-4">
                     <div class="breadcrumbs">
                         <ul>
-                            <li><a href="#">Home</a></li>
+                            <li><a href="#">Category</a></li>
                             <li>{{$category->name}}</li>
                         </ul>
                     </div>
